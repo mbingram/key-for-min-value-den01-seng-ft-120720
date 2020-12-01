9ab7c04fa7f731df99e01aee5cb913170629c64b
charming-input-2976 <mbingramm@gmail.com>
2020-12-01 01:14:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  current_minimun = 
+  current_minimun = nil
+  minimum_key = nil
+  
+  name_hash.each do |key, value|
+    if current_minimun == nil
+      current_minimum = value
+      minimun_key = key
+    else
+      if current_minimum > value
+        current_minimun = value
+        minimun_key = key
+      end
+    end
+  minimun_key
 end
