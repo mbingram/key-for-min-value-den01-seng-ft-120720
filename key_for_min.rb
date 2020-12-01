17a99a32154f807060241f193a8549bf821e80ea
charming-input-2976 <mbingramm@gmail.com>
2020-12-01 01:15:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,16 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  current_minimun = nil
+  current_minimum = nil
   minimum_key = nil
   
   name_hash.each do |key, value|
-    if current_minimun == nil
+    if current_minimum == nil
       current_minimum = value
-      minimun_key = key
+      minimum_key = key
     else
       if current_minimum > value
-        current_minimun = value
+        current_minimum = value
         minimun_key = key
       end
     end
