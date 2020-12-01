866bb2044000b04e1095c1d0619a20e9acfb7fb4
charming-input-2976 <mbingramm@gmail.com>
2020-12-01 01:17:45 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  current_minimum = nil
+  minimum_key = nil
+  
+  name_hash.each do |key, value|
+    if current_minimum == nil
+      current_minimum = value
+      minimum_key = key
+    else
+      if current_minimum > value
+        current_minimum = value
+        minimum_key = key
+      end
+    end
+  end
+  minimum_key
 end
